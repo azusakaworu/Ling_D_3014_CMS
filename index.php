d8f986134402cc5a35475421086cfb469d99948c
Ling Ding <azusakaworu@gmail.com>
2019-04-02 17:22:23 -0400

    updtas

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
-<?php require_once('admin/scripts/config.php');
-if(isset($_GET['filter'])){
-	$tbl = 'tbl_products';
-	$tbl_2 = 'tbl_categories';
-	$tbl_3 = 'tbl_prod_cate';
-	$col = 'products_id';
-	$col_2 = 'cate_id';
-	$col_3 = 'cate_name';
-	$filter = $_GET['filter'];
-	$results = filterResults($tbl,$tbl_2,$tbl_3,$col,$col_2,$col_3,$filter);
-}else{
-	$results = getAll('tbl_products');
+<?php require_once 'admin/scripts/config.php';
+if (isset($_GET['filter'])) {
+    $tbl     = 'tbl_products';
+    $tbl_2   = 'tbl_categories';
+    $tbl_3   = 'tbl_prod_cate';
+    $col     = 'products_id';
+    $col_2   = 'cate_id';
+    $col_3   = 'cate_name';
+    $filter  = $_GET['filter'];
+    $results = filterResults($tbl, $tbl_2, $tbl_3, $col, $col_2, $col_3, $filter);
+} else {
+    $results = getAll('tbl_products');
 }
 ?>
 
@@ -22,25 +22,35 @@ if(isset($_GET['filter'])){
 
 </head>
 <body>
-	
+
 	<!-- <h1 class="hide">Sport Check</h1> -->
-	
-<?php include('templates/header.html'); ?>
+
+<?php include 'templates/header.html';?>
 
 
 
-	<div>
-<?php while($row = $results->fetch(PDO::FETCH_ASSOC)):?>
 
- <a href="details.php?id=<?php echo $row['products_id'];?>">Read More</a>
-<img src="images/<?php echo $row['products_img'];?>" 
-	 alt="<?php echo $row['products_name'];?>">
-	 <h2><?php echo $row['products_name'];?></h2>
-	 <p>Price: $<?php echo $row['products_price'];?></p>
-	
-<?php endwhile;?>
-	</div>
+<section id="productImages">
+<?php
+$count = 1;
+while ($row = $results->fetch(PDO::FETCH_ASSOC)):
+?>
+	<section class="<?php echo $count % 3 === 0 ? 'productImgLast' : 'productImg'; ?> ">
 
-<?php include('templates/footer.html');?>
+            <img src="images/<?php echo $row['products_img']; ?>"
+	 alt="<?php echo $row['products_name']; ?>">
+	 <a href="details.php?id=<?php echo $row['products_id']; ?>">Read More</a>
+	 <h2><?php echo $row['products_name']; ?></h2>
+	 <p>Price: $<?php echo $row['products_price']; ?></p>
+     </section>
+<?php
+$count++;
+endwhile;?>
+</section>
+
+
+
+<?php include 'templates/footer.html';?>
+
 </body>
 </html>
